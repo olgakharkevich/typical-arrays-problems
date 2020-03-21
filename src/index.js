9d9ab7708af164c4263c91e2f403faa0f9949527
olgakharkevich <Olga.Kharkevich@gmail.com>
2020-03-21 14:34:18 +0300

    fix: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (_arraynotempty(array)) ? Math.min.apply(null, array) : 0;
 }
 
 exports.max = function max (array) {
+  if (_arraynotempty(array)) {
+    return Math.max.apply(null, array);
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (_arraynotempty(array)) {
+    return (array.reduce(function(a, b) {
+      return a+b}))/array.length;
+  }
   return 0;
+}
+
+function _arraynotempty(array){
+  if (! Array.isArray(array)) {
+    return false;
+  }
+  else if (array.length) {
+    return true
+  }
+  return false;
 }
